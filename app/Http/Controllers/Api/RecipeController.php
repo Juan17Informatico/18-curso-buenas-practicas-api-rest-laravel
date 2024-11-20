974c77ec00fcf1dd16b4a5145326fd8e089900fe
JuanCM <juancampuzano2356@gmail.com>
2024-11-20 10:25:55 -0500

    Clase #10 Finalizada - Recursos anidados o multinivel:
    Creación de dos recursos más para recipe y tag
    - Se modifican los controladores agregando los recursos y colecciones para mostrar correctamente la información
    - Se anida un recurso dentro de otro para ser consistente en los datos que se le muestran al usuario

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
     public function index()
-    {
-        return Recipe::with('category', 'tags', 'user')->get();
+    {   
+        $recipes = Recipe::with('category', 'tags', 'user')->get();
+        return RecipeResource::collection($recipes);
     }
 
     public function store(Recipe $recipe){ }
 
     public function show(Recipe $recipe)
     {
-        return $recipe->load('category', 'tags', 'user');
+        $recipe = $recipe->load('category', 'tags', 'user');
+        return new RecipeResource($recipe);
     }
 
     public function update(Recipe $recipe){ }
