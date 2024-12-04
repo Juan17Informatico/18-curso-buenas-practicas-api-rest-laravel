bfeb7f40acd9779289f1a5b596a16f2137ef90ba
JuanCM <juancampuzano2356@gmail.com>
2024-12-04 14:45:41 -0500

    Clase #26 Finalizada

--- tests/Feature/RecipeTest.php
+++ tests/Feature/RecipeTest.php
@@ -2,19 +2,91 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class RecipeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+        Category::factory()->create();
+
+        $recipes = Recipe::factory(2)->create();
+
+        $response = $this->getJson('/api/recipes');
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'type',
+                        'attributes' => ['title', 'description'],
+                    ]
+                ]
+            ]);
     }
+
+    public function test_store(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+        
+        Category::factory()->create();
+        Tag::factory()->create();
+
+        $recipe = Recipe::factory()->create();
+
+        $response = $this->getJson('/api/recipes/' . $recipe->id);
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'type',
+                    'attributes' => ['title', 'description'],
+                ]
+            ]);
+    }
+
+    public function test_show(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+        Category::factory()->create();
+
+        $recipe = Recipe::factory()->create();
+
+        $response = $this->getJson('/api/recipes/' . $recipe->id);
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'type',
+                    'attributes' => ['title', 'description'],
+                ]
+            ]);
+    }
+
+    public function test_destroy(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+        Category::factory()->create();
+
+        $recipe = Recipe::factory()->create();
+
+        $response = $this->deleteJson('/api/recipes/' . $recipe->id);
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+    }
+
 }
